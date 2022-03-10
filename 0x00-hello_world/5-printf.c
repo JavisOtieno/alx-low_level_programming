3030dd0ee9cfb24123ed0d432891b0d87d38709f
Javis <javisotieno@gmail.com>
2022-03-10 21:35:35 +0300

    this is the shit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description : prints "With proper grammar, but the outcome is a piece of art,"
+ * Description : printf this time
  * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
