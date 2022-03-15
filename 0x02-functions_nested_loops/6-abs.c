12aa464696e9c05dd3d04da6d3b6e448fd15f96e
Javis <javisotieno@gmail.com>
2022-03-15 18:32:54 +0300

    5 sign c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*_abs: Entry point
+*_abs - Entry point
 *Description: find out if absolute
 *@n: number on which absolute value is calculated
 *Return: absolute value of integer
