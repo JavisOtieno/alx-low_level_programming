dc2a8b080308510970b2243d8b2679ecee6d9ec6
Javis <javisotieno@gmail.com>
2022-03-15 18:36:14 +0300

    5 sign c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - Entry point
+*Description: find out last digit
+*@n: number on which last digit is obtained
+*Return: last digit of number
+*/
+int print_last_digit(int n)
+{
+return n%10;
+}
+
