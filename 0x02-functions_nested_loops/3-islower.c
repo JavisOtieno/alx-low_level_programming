2cd111efbf5315ae2584462bcc525fdbb4f5b940
Javis <javisotieno@gmail.com>
2022-03-15 14:58:31 +0300

    3 lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-/** 
+/**
 *main - Entry Point
 *Description : function to check if letter is lower
 *Return : 0
 */
-int main(void){
-
+int _islower(int c)
+{
+if (c <= 65 || c >= 97){
+return 1;
 }
-bool _islower(int c){
-    if(c<=65 || c>=97){
-        return 1;
-    }else{
-        return 0;
-    }
+else{
+return 0;
+}
 }
 
