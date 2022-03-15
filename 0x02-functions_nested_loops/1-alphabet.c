6426bbc19cc599c14f028dbe09971877d70ea1c2
Javis <javisotieno@gmail.com>
2022-03-15 12:05:55 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+/**
+*main - Entry point
+*Description : Prints the alphabet
+*Return : 0
+*/
+int main(void)
+{
+    for(int n=97;n<=122;n++)
+    {
+        printf("%c",n);
+    }
+    printf("\n");
+}
