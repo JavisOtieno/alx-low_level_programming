89b77ba29b6ffd5c4778dd780e393357e6048694
Javis <javisotieno@gmail.com>
2022-03-15 14:50:06 +0300

    2 print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* print_alphabet_x10 - Entry point
 * Description:  print alphabet in lowercase 10 times
 * Returns nothing
 */
 
 void print_alphabet_x10(void)
 {
-int i=0;
-while(i<10)
+int i = 0;
+while (i < 10)
 {
 int n = 97;
-while(n <= 122){
+while (n <= 122)
+{
 printf("%c", n);
 n++;
 }
