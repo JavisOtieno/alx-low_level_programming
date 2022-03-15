3e235c50b3a7a41b316438462d65c534ddad70bf
Javis <javisotieno@gmail.com>
2022-03-15 14:47:04 +0300

    putchar fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Description : print alphabet in lowercase 10 times
-* Return : 0
+* Description:  print alphabet in lowercase 10 times
+* Returns nothing
 */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-    int i=0;
-    while(i<10){
-
-    for(int n=97; n<=122; n++){
-        printf("%c",n);
-    }
-    printf("\n");
-
-    i++;
-
-    }
-
+int i=0;
+while(i<10)
+{
+int n = 97;
+while(n <= 122){
+printf("%c", n);
+n++;
+}
+printf("\n");
+i++;
+}
 }
