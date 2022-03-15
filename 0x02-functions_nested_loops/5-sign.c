90008d66dd8dfb5c9ba46e795af513237ff64a26
Javis <javisotieno@gmail.com>
2022-03-15 15:42:00 +0300

    5 sign c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,16 +4,18 @@
  *@n: number to be checked
  *Return: 1 if positive, 0 if zero and -1 if negative
  */
-int print_sign(int n){
-if (n>0)
+int print_sign(int n)
 {
-return(1);
+if (n > 0)
+{
+return (1);
 }
-else if(n=0)
+else if (n == 0)
 {
-return(0);
-}else
-{
-return(-1)
+return (0);
 }
-
+else
+{
+return (-1);
+}
+}
