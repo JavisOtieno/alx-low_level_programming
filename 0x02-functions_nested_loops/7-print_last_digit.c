2c454d233e78e86bf0d7fe4176471f0d0821e46b
Javis <javisotieno@gmail.com>
2022-03-15 18:50:19 +0300

    7 print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,12 @@
 */
 int print_last_digit(int n)
 {
-if(n<0)
+if(n < 0)
 {
-n=-n;
+n = -n;
 }
-int last_digit = n%10;
+int last_digit;
+last_digit = n%10;
 _putchar(last_digit + '0');
 return last_digit;
 }
