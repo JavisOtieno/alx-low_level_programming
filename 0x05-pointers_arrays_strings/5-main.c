6a540f3c4dd738c361b29a2401443ea563336a76
Javis <javisotieno@gmail.com>
2022-03-21 14:31:16 +0300

    Alx

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for Alx School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s[10] = "Holberton";
+	char s[10] = "Alx";
 
 	printf("%s\n", s);
 	rev_string(s);
